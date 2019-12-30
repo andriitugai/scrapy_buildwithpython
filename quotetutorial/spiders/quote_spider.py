fd391a3fd3fd38d09e1733d2a072e6a10929ab3b
Andrii Tugai <andrii.tugai@gmail.com>
2019-12-30 14:41:34 +0200

    First commit

---
+++ quotetutorial/spiders/quote_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+from ..items import QuotetutorialItem
+
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'http://quotes.toscrape.com'
+    ]
+
+    def parse(self, response):
+        items = QuotetutorialItem()
+
+        all_div_quotes = response.css("div.quote")
+
+        for quote in all_div_quotes:
+            title = quote.css('span.text::text').extract()
+            author = quote.css('.author::text').extract()
+            tags = quote.css(".tag::text").extract()
+
+            items['title'] = title
+            items['author'] = author
+            items['tags'] = tags
+
+            yield items
